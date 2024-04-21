7f9e447026d10f7444720ad3af350e0b67ba3d68
nhubaole <okamisameko7123@gmail.com>
2024-04-21 16:53:03 +0700

    sample crud

--- src/main/java/com/adventour/web/controller/TourController.java
+++ src/main/java/com/adventour/web/controller/TourController.java
@@ -1,11 +1,15 @@
 package com.adventour.web.controller;
 
 import com.adventour.web.dto.TourDto;
+import com.adventour.web.models.Tour;
 import com.adventour.web.service.TourService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -32,7 +36,22 @@ public class TourController {
 
     @GetMapping("/all-tour")
     public String allTour(Model model){
+        List<TourDto> tourDtos = tourService.findAllTours();
+        model.addAttribute("tours", tourDtos);
         return "/pages/all-tour";
+    }
+
+    @PostMapping("/add-tour")
+    public String saveTour(@ModelAttribute("tour") Tour tour){
+        tourService.saveTour(tour);
+        return "redirect:/all-tour";
+    }
+
+    @GetMapping("/all-tour/{tourId}")
+    public String tourDetail(@PathVariable("tourId") long tourId, Model model){
+        TourDto tourDto = tourService.findByTourId(tourId);
+        model.addAttribute("tour", tourDto);
+        return "/pages/tour-info";
     }
     @GetMapping("/ticket-detail")
     public String viewTicketDetail(Model model){
@@ -79,15 +98,15 @@ public class TourController {
     @GetMapping("/add-new-trip")
     public String addNewTrip(Model model){return "/pages/add-new-trip";}
 
-
-    @GetMapping("/tour-info")
-    public String tourInfo(Model model) {return "/pages/tour-info";}
-
     @GetMapping("/tour-maps")
     public String tourMap(Model model){return "/pages/tour-map";}
 
     @GetMapping("/add-tour")
-    public String addNewTour(Model model){return "/pages/add-tour";}
+    public String addNewTour(Model model){
+        Tour tour = new Tour();
+        model.addAttribute("tour", tour);
+        return "/pages/add-tour";
+    }
 
     @GetMapping("/tour-detail")
     public String tourDetail(Model model){return "/pages/tour-detail";}
