96825726ce3c3d3c0246c93a9919aa30810eac61
Huỳnh Ngọc Ý Nhi <21522423@gm.uit.edu.vn>
2024-05-03 15:41:21 +0700

    fix bug

--- src/main/java/com/adventour/web/controller/LoginController.java
+++ src/main/java/com/adventour/web/controller/LoginController.java
@@ -27,29 +27,7 @@ public class LoginController {
 
     @GetMapping("/")
     public String home(Model model){
-        //save customer
-//        CustomerDto customerDto = new CustomerDto();
-//        customerDto.setId(2L);
-//        customerDto.setNameCustomer("Nhi Huynh");
-//        customerDto.setPhoneNumber("0364065040");
-//        customerDto.setEmail("nhih0403@gmail.com");
-//        customerDto.setDateOfBirth(LocalDate.of(2024, 4, 24));
-//        customerDto.setCccd("123456789");
-//        customerDto.setAddressCustomer("70 Võ Văn Ngân, Thủ Đức");
-//        customerDto.setNationality("England");
-//        if(customerService.updateCustomer(customerDto) == null) {
-//            logger.info("tr ve null");
-//        }
-//        else {
-//            List<CustomerDto> customers = customerService.getListCustomer();
-//            logger.info("List of customers: {}" + customers);
-//        }
-//
-//        CustomerDto customerDto1 = customerService.findById(2L);
-//        logger.info(String.valueOf(customerDto1));
 
-        List<CustomerDto> cus = customerService.searchCustomer("0914021632" );
-        logger.info("List search customer"+ cus );
 
         return "/index";
     }
