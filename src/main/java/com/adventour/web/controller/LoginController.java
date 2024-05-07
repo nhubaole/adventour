7db5c2ad02a998428457aa0a15dc98eced4f0a02
Huỳnh Ngọc Ý Nhi <21522423@gm.uit.edu.vn>
2024-05-07 16:26:53 +0700

    fix

--- src/main/java/com/adventour/web/controller/LoginController.java
+++ src/main/java/com/adventour/web/controller/LoginController.java
@@ -1,5 +1,8 @@
 package com.adventour.web.controller;
 
+import com.adventour.web.dto.LocationDto;
+import com.adventour.web.models.Location;
+import com.adventour.web.service.LocationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,40 +20,14 @@ import java.util.List;
 @Controller
 public class LoginController {
 
-    private CustomerService customerService;
+
     private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
     @Autowired
-    public LoginController( CustomerService customerService) {
-        this.customerService = customerService;
-
+    public LoginController( ) {
     }
 
     @GetMapping("/")
     public String home(Model model){
-        //save customer
-//        CustomerDto customerDto = new CustomerDto();
-//        customerDto.setId(2L);
-//        customerDto.setNameCustomer("Nhi Huynh");
-//        customerDto.setPhoneNumber("0364065040");
-//        customerDto.setEmail("nhih0403@gmail.com");
-//        customerDto.setDateOfBirth(LocalDate.of(2024, 4, 24));
-//        customerDto.setCccd("123456789");
-//        customerDto.setAddressCustomer("70 Võ Văn Ngân, Thủ Đức");
-//        customerDto.setNationality("England");
-//        if(customerService.updateCustomer(customerDto) == null) {
-//            logger.info("tr ve null");
-//        }
-//        else {
-//            List<CustomerDto> customers = customerService.getListCustomer();
-//            logger.info("List of customers: {}" + customers);
-//        }
-//
-//        CustomerDto customerDto1 = customerService.findById(2L);
-//        logger.info(String.valueOf(customerDto1));
-
-        List<CustomerDto> cus = customerService.searchCustomer("0914021632" );
-        logger.info("List search customer"+ cus );
-
         return "/index";
     }
 }
