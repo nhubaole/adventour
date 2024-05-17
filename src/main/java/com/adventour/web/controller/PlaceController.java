f49e724e4b68249d96b6203452815b8dd76ff771
nhubaole <okamisameko7123@gmail.com>
2024-05-18 01:05:36 +0700

    add place management feature

--- src/main/java/com/adventour/web/controller/PlaceController.java
+++ src/main/java/com/adventour/web/controller/PlaceController.java
@@ -1,14 +1,20 @@
 package com.adventour.web.controller;
 
+import com.adventour.web.dto.LocationDto;
+import com.adventour.web.dto.TourDto;
+import com.adventour.web.models.Schedule;
+import com.adventour.web.models.Tour;
 import com.adventour.web.service.BucketService;
 import com.adventour.web.service.LocationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class PlaceController {
@@ -21,27 +27,54 @@ public class PlaceController {
         this.bucketService = bucketService;
     }
     @GetMapping("/all-place")
-    public String allPlace(Model model){return "/pages/all-place";}
+    public String allPlace(Model model,  @Param("keyword") String keyword){
+        List<LocationDto> locationDtos = new ArrayList<>();
+        if (keyword == null) {
+            locationDtos =  placeService.findAllLocation();
+        } else {
+            locationDtos = placeService.searchLocation(keyword);
+            model.addAttribute("keyword", keyword);
+        }
+        model.addAttribute("places", locationDtos);
+        return "/pages/all-place";
+    }
+
+    @GetMapping("/all-place/{placeId}/delete")
+    public String deleteTour(@PathVariable("placeId") long placeId){
+        placeService.deleteLocation(placeId);
+        return "redirect:/all-place";
+    }
 
     @GetMapping("/place-detail")
     public String placeDetail(Model model){return "/pages/place-detail";}
 
     @GetMapping("/add-new-place")
-    public String addPlace(Model model){return "/pages/add-new-place";}
-
-    @PostMapping("/add-new-place")
-    public String uploadFile1(Model model, @RequestParam("file") MultipartFile file) {
-        String message = "";
-        try {
-            String fileUrl = bucketService.uploadFile(file);
-            message = "Uploaded the file successfully: " + file.getOriginalFilename();
-            model.addAttribute("message", message);
-            model.addAttribute("fileUrl", fileUrl);
-        } catch (Exception e) {
-            message = "Could not upload the file: " + file.getOriginalFilename() + ". Error: " + e.getMessage();
-            model.addAttribute("message", message);
+    public String addPlace(Model model){
+        LocationDto place = new LocationDto();
+        place.setImages(new String[]{});
+        model.addAttribute("place", place);
+        return "/pages/add-new-place";
         }
 
-        return "/pages/add-new-place";
+    @PostMapping("/add-new-place")
+    public String savePlace(@ModelAttribute("place") LocationDto place){
+        placeService.addNewLocation(place);
+        return "redirect:/all-place";
     }
+
+//    @PostMapping("/add-new-place")
+//    public String uploadFile1(Model model, @RequestParam("file") MultipartFile file) {
+//        String message = "";
+//        try {
+//            String fileUrl = bucketService.uploadFile(file);
+//            message = "Uploaded the file successfully: " + file.getOriginalFilename();
+//            model.addAttribute("message", message);
+//            model.addAttribute("fileUrl", fileUrl);
+//        } catch (Exception e) {
+//            message = "Could not upload the file: " + file.getOriginalFilename() + ". Error: " + e.getMessage();
+//            model.addAttribute("message", message);
+//        }
+//
+//        return "/pages/add-new-place";
+//    }
 }
