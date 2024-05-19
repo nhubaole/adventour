d4aa79ea39cb576c59f138e027e929c4f981b733
Thanh-Dat <133691067+Datdtfrontend@users.noreply.github.com>
2024-05-19 23:18:06 +0700

    Update

--- src/main/java/com/adventour/web/controller/CustomerController.java
+++ src/main/java/com/adventour/web/controller/CustomerController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 
@@ -27,7 +28,18 @@ public class CustomerController {
     }
 
     @GetMapping("/add-new-customer")
-    public String addNewCustomer(Model model){ return "/pages/add-new-customer";}
+    public String addNewCustomer(Model model){
+        Customer customer = new Customer();
+        model.addAttribute("customer", customer);
+        return "/pages/add-new-customer";
+    }
+
+    @PostMapping("/add-new-customer")
+    public String saveCustomer(@ModelAttribute("customer") CustomerDto customer){
+        customerService.addNewCustomer(customer);
+        return "/pages/customer";
+    }
+
 
     @GetMapping("/profile")
     public String profile(Model model){return "/pages/profile";}
