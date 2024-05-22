f39a338ab701c552abe122b9b0bcac94590614bb
Huỳnh Ngọc Ý Nhi <21522423@gm.uit.edu.vn>
2024-05-22 23:13:40 +0700

    tesst

--- src/main/java/com/adventour/web/controller/LoginController.java
+++ src/main/java/com/adventour/web/controller/LoginController.java
@@ -37,8 +37,6 @@ public class LoginController {
         this.bookingService = bookingService;
     }
 
-
-
     @GetMapping("/")
     public String home(Model model){
 
@@ -63,56 +61,67 @@ public class LoginController {
 
 //        tripDto.setEndDate(endDate);
 
-        //Trip trip = tripService.addNewTrip(tripDto);
+//        Trip trip = tripService.addNewTrip(tripDto);
 
-//        TripDto tripDto = tripService.getTripDetail(2L);
-//
-//
-//        CustomerDto customerDto = customerService.findById(1L);
-//
-//        BookingDto bookingDto = new BookingDto();
-//
-//        LocalDateTime timeBooking = LocalDateTime.now();
-//        bookingDto.setBookingDate(timeBooking);
-//
-//        bookingDto.setNumberBaby(2);
-//        bookingDto.setNumberAdult(10);
-//        bookingDto.setNumberChildren(4);
-//
-//        bookingDto.setTripDto(tripDto);
-//        bookingDto.setCustomerDto(customerDto);
-//
-//
-//        Set<PassengerDto> passengerDtos = new HashSet<>();
-//
-//        passengerDtos.add(new PassengerDto("Nguyen Van A", "001", true, "Adult", LocalDate.of(1980, 1, 1)));
-//        passengerDtos.add(new PassengerDto("Tran Thi B", "002", false, "Adult", LocalDate.of(1985, 2, 2)));
-//        passengerDtos.add(new PassengerDto("Le Van C", "003", true, "Child", LocalDate.of(2010, 3, 3)));
-//        passengerDtos.add(new PassengerDto( "Pham Thi D", "004", false, "Child", LocalDate.of(2012, 4, 4)));
-//        passengerDtos.add(new PassengerDto( "Hoang Van E", "005", true, "Adult", LocalDate.of(1990, 5, 5)));
-//
-//
-//        bookingDto.setPassengerDtos(passengerDtos);
-//        // Print the passengers set
-//
-//        Set<PaymentInformationDto> paymentInformationDtoSet = new HashSet<>();
-//        PaymentInformationDto paymentInformationDto = new PaymentInformationDto();
-//
-//        paymentInformationDto.setPaymentMethod(PaymentMethod.BANKTRANSFER);
-//        paymentInformationDto.setPaymentTime(LocalDateTime.now());
-//        paymentInformationDto.setAmountOfMoney(200000);
-//        paymentInformationDtoSet.add(paymentInformationDto);
-//
-//        bookingDto.setPaymentInformationDtos(paymentInformationDtoSet);
-//       logger.info(bookingService.addNewBooking(bookingDto).toString());
-
-        BookingDto  bookingDto = bookingService.findById(16L);
-
-        logger.info(String.valueOf(bookingDto.getAmountPaid()));
-
-        logger.info(bookingDto.getPassengerDtos().toArray().toString());
+        TripDto tripDto = tripService.getTripDetail(2L);
 
 
+        CustomerDto customerDto = new CustomerDto();
+        customerDto.setNameCustomer("Huỳnh Ngoc Y");
+        customerDto.setAddressCustomer("Dh CNTT");
+        customerDto.setCccd("06666784345");
+        customerDto.setMale(true);
+        customerDto.setEmail("nhih0406@gmail.com");
+        customerDto.setNationality("England");
+
+        LocalDate dob = LocalDate.of(2004, 4, 12);
+        customerDto.setDateOfBirth(dob);
+        customerDto.setPhoneNumber("0364566089");
+
+
+        BookingDto bookingDto = new BookingDto();
+
+        LocalDateTime timeBooking = LocalDateTime.now();
+        bookingDto.setBookingDate(timeBooking);
+
+        bookingDto.setNumberBaby(1);
+        bookingDto.setNumberAdult(14);
+        bookingDto.setNumberChildren(4);
+
+        bookingDto.setTripDto(tripDto);
+        bookingDto.setCustomerDto(customerDto);
+
+
+        Set<PassengerDto> passengerDtos = new HashSet<>();
+
+        passengerDtos.add(new PassengerDto("Nguyen Van F", "001", true, "Adult", LocalDate.of(1980, 1, 1)));
+        passengerDtos.add(new PassengerDto("Tran Thi G", "002", false, "Adult", LocalDate.of(1985, 2, 2)));
+        passengerDtos.add(new PassengerDto("Le Van H", "003", true, "Child", LocalDate.of(2010, 3, 3)));
+        passengerDtos.add(new PassengerDto( "Pham Thi I", "004", false, "Child", LocalDate.of(2012, 4, 4)));
+        passengerDtos.add(new PassengerDto( "Hoang Van K", "005", true, "Adult", LocalDate.of(1990, 5, 5)));
+
+
+        bookingDto.setPassengerDtos(passengerDtos);
+        // Print the passengers set
+
+        Set<PaymentInformationDto> paymentInformationDtoSet = new HashSet<>();
+        PaymentInformationDto paymentInformationDto = new PaymentInformationDto();
+
+        paymentInformationDto.setPaymentMethod(PaymentMethod.BANKTRANSFER);
+        paymentInformationDto.setPaymentTime(LocalDateTime.now());
+        paymentInformationDto.setAmountOfMoney(500000);
+        paymentInformationDtoSet.add(paymentInformationDto);
+
+        bookingDto.setPaymentInformationDtos(paymentInformationDtoSet);
+       logger.info(bookingService.addNewBooking(bookingDto).toString());
+
+//        List<BookingDto>  bookingDto = bookingService.findById(16L);
+//
+//        for(BookingDto pass: bookingDto){
+//            logger.info(pass.toString());
+//        }
+//
+//        logger.info(bookingService.findById(17L).toString());
         return "/index";
     }
 }
